ebcdf8fb766639f663b7a4c0f077c23b9660f4ba
Noah Friedman <noahfriedman2@gmail.com>
2020-10-24 12:37:27 -0400

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import {EnzymeAdapter} from "enzyme";
+
+declare module "@wojtekmaj/enzyme-adapter-react-17" {
+  export default class ReactSeventeenAdapter extends EnzymeAdapter {
+    constructor () {};
+  };
+}
