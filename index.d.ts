5d95419f74628d64e4a5bff1898dffd24c11f4d9
Matthew Marcuccio <matthew@marcuccio.com>
2020-11-20 09:29:04 -0600

    Fix type declaration (#4)

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@ import { EnzymeAdapter } from 'enzyme';
 
 declare module "@wojtekmaj/enzyme-adapter-react-17" {
   export default class ReactSeventeenAdapter extends EnzymeAdapter {
-    constructor () {};
+    constructor();
   }
 }
