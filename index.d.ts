bab0bd0a3b951f4996bcfbc6548fede8c79f6665
Sylvain Desvé <sylvain.desve@gmail.com>
2021-02-23 14:15:16 +0100

    Fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,10 @@
 import { EnzymeAdapter } from 'enzyme';
 
-declare module "@wojtekmaj/enzyme-adapter-react-17" {
-  export default class ReactSeventeenAdapter extends EnzymeAdapter {
-    constructor();
-  }
+declare class ReactSeventeenAdapter extends EnzymeAdapter {
 }
+
+declare namespace ReactSeventeenAdapter {
+}
+
+export = ReactSeventeenAdapter;
+
