cbafb5fb6d7e7349d9c8cfbcd12109fb84dd5bc7
paulclijsters <paulclijsters@users.noreply.github.com>
2021-03-22 11:42:17 +0100

    Ensure recursive calls to `displayNameOfNode` uses the adapter's version of the method (#15)
    Co-authored-by: Paul Clijsters <pclijsters@astrata.eu>

--- src/ReactSeventeenAdapter.js
+++ src/ReactSeventeenAdapter.js
@@ -807,6 +807,7 @@ class ReactSeventeenAdapter extends EnzymeAdapter {
   displayNameOfNode(node) {
     if (!node) return null;
     const { type, $$typeof } = node;
+    const adapter = this;
 
     const nodeType = type || $$typeof;
 
@@ -830,13 +831,13 @@ class ReactSeventeenAdapter extends EnzymeAdapter {
       case ContextProvider || NaN: return 'ContextProvider';
       case Memo || NaN: {
         const nodeName = displayNameOfNode(node);
-        return typeof nodeName === 'string' ? nodeName : `Memo(${displayNameOfNode(type)})`;
+        return typeof nodeName === 'string' ? nodeName : `Memo(${adapter.displayNameOfNode(type)})`;
       }
       case ForwardRef || NaN: {
         if (type.displayName) {
           return type.displayName;
         }
-        const name = displayNameOfNode({ type: type.render });
+        const name = adapter.displayNameOfNode({ type: type.render });
         return name ? `ForwardRef(${name})` : 'ForwardRef';
       }
       case Lazy || NaN: {
